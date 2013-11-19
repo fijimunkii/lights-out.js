f118d4b6a58e42a044e878c0d0dbec139d080673
Harrison Powers <fijimunkii@gmail.com>
2013-11-18 21:10:55 -0500

    css inset box shadows

--- js/game.js
+++ js/game.js
@@ -76,7 +76,7 @@ $(function() {
 
   function newGame() {
 
-    var randomNumber=Math.floor(Math.random()*11);
+    var randomNumber=Math.floor(Math.random()*10);
 
     if (randomNumber === 0) {
       $('#1-5').addClass('on');
